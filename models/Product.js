36e04abf2b0bcc51d28807ecc518216436fea1ac
Mary90272 <lefebvre.mary90272@gmail.com>
2023-01-28 15:28:10 -0800

    update product.js

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,11 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model { }
+class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
+  {  // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -21,10 +20,10 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
-        isDecimal: true
+        isDecimal: true,
       }
     },
     stock: {
@@ -32,17 +31,16 @@ Product.init(
       allowNull: false,
       defaultValue: 10,
       validate: {
-        isInt: true
+        isNumeric: true,
       }
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
-        key: 'id'
-      }
-
-    }
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
